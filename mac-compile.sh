b434fdab07d88c855ae93a4e626082dba8f83865
Zachary Rohrbach <accounts@rohrbach.tech>
2020-02-21 00:32:55 -0500

    Fixed this ti work correctly.

--- mac-compile.sh
+++ mac-compile.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 mkdir pdf
-pdflatex -halt-on-error -output-directory pdf main.tex
+pdflatex -halt-on-error -output-directory ./pdf main.tex
 mv pdf/main.pdf Zachary-Rohrbach-Resume.pdf
-open Zachary-Rohrbach-Resume.pdf
 rm -rf pdf
+open Zachary-Rohrbach-Resume.pdf
